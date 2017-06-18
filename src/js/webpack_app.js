b44810f45878dd0c65e325f0fdc1d735ed5918df
Wojciech Dudek <wdudek82@gmail.com>
2017-06-19 00:55:36 +0200

    Fixed subsribe/unsubscribe in Observable

--- src/js/webpack_app.js
+++ src/js/webpack_app.js
@@ -4,15 +4,32 @@ import Rx from 'rxjs/Rx';
 import '../css/style.css';
 import people from './people';
 
+
+$.each(people, (key, value) => {
+    $('body').append('<h1>'+people[key].name+'</h1>');
+});
+
+people.forEach(e => console.log(e.name));
+
+
+// RxJS
 let button = document.getElementById('button');
 
 let Observable = Rx.Observable;
 let clicks = Observable.fromEvent(button, 'click');
 
-clicks.forEach(_ => alert('Hello!'));
+let subscription =
+    clicks.subscribe(
+        function onNext(e) {
+            alert('Hello!');
+            console.log('Hello!');
+            // subscription.unsubscribe();
+        },
+        function onError(error) {
+            console.log(error);
+        },
+        function onCompleted() {
+            console.log('done!');
+        }
+);
 
-people.forEach(e => console.log(e.name));
-
-$.each(people, (key, value) => {
-    $('body').append('<h1>'+people[key].name+'</h1>');
-});
