ad8691dd225d93dad580246d466d6820eaf76c36
Wojciech Dudek <wdudek82@gmail.com>
2017-06-22 00:06:50 +0200

    Separated webpack config and babel config, added react hot reload, rewrited conter as a react component

---
+++ src/js/react_redux_sandbox_1.js
@@ -0,0 +1,74 @@
+import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import expect from 'expect';
+
+// import { createStore } from './redux_sandbox_2';
+
+
+// Reducer
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const Counter = ({ value, onIncrement, onDecrement }) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
+const store = createStore(counter);
+
+const render = () => {
+    ReactDOM.render(
+        <Counter value={store.getState()} />,
+        document.getElementById('counter')
+    );
+};
+render();
+
+store.subscribe(render);
+
+
+const increment = document.getElementById('inc');
+increment.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' })
+});
+
+const decrement = document.getElementById('dec');
+decrement.addEventListener('click', () => {
+    store.dispatch({ type: 'DECREMENT' })
+});
+
+
+// Tests
+expect(
+    counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+    counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+expect(
+    counter(2, { type: 'DECREMENT' })
+).toEqual(1);
+
+expect(
+    counter(1, { type: 'DECREMENT' })
+).toEqual(0);
+
+expect(
+    counter(undefined, {})
+).toEqual(0);
+
+console.log('Tests passed!');
