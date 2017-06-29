cafa6e2716c8722bba4cd62d0971b97e868eeff0
Wojciech Dudek <wdudek82@gmail.com>
2017-06-29 23:38:14 +0200

    Learning Promises

--- src/js_sandbox/promises.js
+++ src/js_sandbox/promises.js
@@ -1,3 +1,17 @@
-/**
- * Created by neevor on 29/06/17.
- */
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (true) {
+            resolve('hello world');
+        } else {
+            reject('no bueno');
+        }
+    }, 2000)
+});
+
+p.then(data => console.log('Success: ' + data));
+p.catch(error => console.log('Error: ' + error));
+
+// p.then(
+//     data => console.log('Success: ' + data),
+//     error => console.log('Error: ' + error)
+// );
